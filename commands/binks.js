48fb70ec30fe006ce773fd25ad4cdf10135273e2
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2024-08-02 01:26:10 -0500

    move commands to separate module

---
+++ commands/binks.js
@@ -0,0 +1,11 @@
+const handleBinks = (message) => {
+  if (message.content.startsWith('!binks')) {
+    const msg = message.content.replace('!binks', '').trim();
+    if (msg.length === 0) {
+      const rv = 'Please provide a question or instruction'
+      return rv;
+    }
+  } else {
+    return msg;
+  }
+}
