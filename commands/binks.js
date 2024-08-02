b36e32de1a608213e9f0d06cdd00bd9deacea6d0
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2024-08-02 06:30:00 -0500

    syntax

--- commands/binks.js
+++ commands/binks.js
@@ -2,7 +2,7 @@ const handleBinks = (message) => {
   if (message.content.startsWith('!binks')) {
     const msg = message.content.replace('!binks', '').trim();
     if (msg.length === 0) {
-      const rv = 'Please provide a question or instruction'
+      const rv = 'Please provide a question or instruction';
       return rv;
     }
   } else {
