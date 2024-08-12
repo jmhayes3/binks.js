20ab2b334af4c4281bdbbc1cec5521681dd9c7c7
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2024-08-12 03:35:32 -0500

    use threads to keep track of chat context

--- commands/core/chat.js
+++ commands/core/chat.js
@@ -1,110 +1,112 @@
 require('dotenv').config();
 
-const { SlashCommandBuilder } = require('discord.js');
+const { GuildTextThreadManager, SlashCommandBuilder } = require('discord.js');
 const { OpenAI } = require("openai");
 
 const openai = new OpenAI({
-	apiKey: process.env['OPENAI_API_KEY'],
+  apiKey: process.env['OPENAI_API_KEY'],
 });
 
 const sleep = (ms) => {
-	return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 const threadCache = {};
 
 const loadThread = (threadId) => {
-	// Replace this in-memory implementation with a database (e.g. DynamoDB, Firestore, Redis)
-	return threadCache[threadId];
+  // Replace this in-memory implementation with a database (e.g. DynamoDB, Firestore, Redis)
+  return threadCache[threadId];
 }
 
 const addThreadToCache = (threadId, openaiThreadId) => {
-	threadCache[threadId] = openaiThreadId;
+  threadCache[threadId] = openaiThreadId;
 }
 
 const terminalStates = ["cancelled", "failed", "completed", "expired"];
 const statusCheckLoop = async (openaiThreadId, runId) => {
-	const run = await openai.beta.threads.runs.retrieve(
-		openaiThreadId,
-		runId
-	);
+  const run = await openai.beta.threads.runs.retrieve(
+    openaiThreadId,
+    runId
+  );
 
-	if (terminalStates.indexOf(run.status) < 0) {
-		await sleep(1000);
-		return statusCheckLoop(openaiThreadId, runId);
-	}
-	console.log("RUN: ", run);
-	console.log("RUN STATUS: ", run.status);
+  if (terminalStates.indexOf(run.status) < 0) {
+    await sleep(1000);
+    return statusCheckLoop(openaiThreadId, runId);
+  }
+  console.log("RUN: ", run);
 
-	return run.status;
+  return run.status;
 }
 
 const addMessage = (threadId, content) => {
-	console.log("Adding message to OpenAI thread: ", content)
-	return openai.beta.threads.messages.create(
-		threadId,
-		{
-			role: "user",
-			content: content,
-		}
-	)
+  console.log("Adding message to OpenAI thread: ", content)
+  return openai.beta.threads.messages.create(
+    threadId,
+    {
+      role: "user",
+      content: content,
+    }
+  )
 }
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('chat')
-		.setDescription('Start a new chat.')
-		.addStringOption((option) => option.setName('message').setDescription('Input text.').setRequired(true))
-		.addStringOption((option) => option.setName('thread').setDescription('Thread ID.').setRequired(false))
-		.addStringOption((option) => option.setName('assistant').setDescription('OpenAI Assistant ID.').setRequired(false)),
-	async execute(interaction) {
-		const sent = await interaction.reply({ content: 'Thinking...', fetchReply: true });
+  data: new SlashCommandBuilder()
+    .setName('chat')
+    .setDescription('Start a new chat.')
+    .addStringOption((option) => option.setName('message').setDescription('Input text.').setRequired(true))
+    .addStringOption((option) => option.setName('thread').setDescription('Thread ID.').setRequired(false))
+    .addStringOption((option) => option.setName('assistant').setDescription('OpenAI Assistant ID.').setRequired(false)),
+  async execute(interaction) {
+    const sent = await interaction.reply({ content: 'Thinking...', fetchReply: true });
 
-		// TODO: Create proper discord thread from thread command option if
-		// it doesn't already exist.
-		// Check threadCache first.
+    // TODO: Create proper discord thread from thread command option if
+    // it doesn't already exist.
+    // Check threadCache first.
 
-		const interactionId = interaction.id;
-		console.log("Interaction ID: ", interactionId);
+    channel = await interaction.client.channels.fetch(process.env.CHANNEL_ID);
+    console.log("Channels: ", channel);
 
-		const message = interaction.options.getString('message');
-		console.log("Message: ", message);
+    const interactionId = interaction.id;
+    console.log("Interaction ID: ", interactionId);
 
-		const assistantId = interaction.options.getString('assistant');
-		console.log("Assistant ID: ", assistantId);
+    const message = interaction.options.getString('message');
+    console.log("Message: ", message);
 
-		const threadId = interaction.options.getString('thread');
-		console.log("Thread ID: ", threadId);
+    const assistantId = interaction.options.getString('assistant');
+    console.log("Assistant ID: ", assistantId);
 
-		if (threadId) {
-			console.log(threadId);
-		}
+    const threadId = interaction.options.getString('thread');
+    console.log("Thread ID: ", threadId);
 
-		let openaiThreadId = loadThread(interactionId);
-		console.log("OpenAI Thread ID: ", openaiThreadId);
+    if (threadId) {
+      console.log(threadId);
+    }
 
-		if (!openaiThreadId) {
-			const thread = await openai.beta.threads.create();
-			openaiThreadId = thread.id;
-			addThreadToCache(interactionId, openaiThreadId);
-		}
+    let openaiThreadId = loadThread(interactionId);
+    console.log("OpenAI Thread ID: ", openaiThreadId);
 
-		await addMessage(openaiThreadId, message);
+    if (!openaiThreadId) {
+      const thread = await openai.beta.threads.create();
+      openaiThreadId = thread.id;
+      addThreadToCache(interactionId, openaiThreadId);
+    }
 
-		const run = await openai.beta.threads.runs.create(
-			openaiThreadId,
-			{ assistant_id: assistantId || process.env.ASSISTANT_ID }
-		)
-		const status = await statusCheckLoop(openaiThreadId, run.id);
-		console.log("Status: ", status);
+    await addMessage(openaiThreadId, message);
 
-		const messages = await openai.beta.threads.messages.list(openaiThreadId);
-		let response = messages.data[0].content[0].text.value;
+    const run = await openai.beta.threads.runs.create(
+      openaiThreadId,
+      { assistant_id: assistantId || process.env.ASSISTANT_ID }
+    )
+    const status = await statusCheckLoop(openaiThreadId, run.id);
+    console.log("Status: ", status);
 
-		// TODO: split up messages
-		response = response.substring(0, 1999)  // Discord message length?
-		console.log("Response: ", response);
+    const messages = await openai.beta.threads.messages.list(openaiThreadId);
+    let response = messages.data[0].content[0].text.value;
 
-		interaction.editReply(`${response} (Latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms)`);
-	},
+    // TODO: split up messages
+    response = response.substring(0, 1999)  // Discord message length?
+    console.log("Response: ", response);
+
+    interaction.editReply(`${response} (Latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms)`);
+  },
 };
