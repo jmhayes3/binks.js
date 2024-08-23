7bcd2804a40cb43c975470b815544882f872ca62
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2024-08-22 21:31:15 -0500

    set initial bot status to online

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		client.user.setPresence({ activities: [], status: "invisible" });
+		client.user.setPresence({ activities: [], status: "online" });
 		// TODO: Reload commands on bot start up.
 	},
 };
