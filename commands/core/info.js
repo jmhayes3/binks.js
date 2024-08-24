c45ec58d1a4abc85e4797efa59f42c220d2630a4
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2024-08-23 21:13:16 -0500

    remove info command

--- commands/core/info.js
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js';
-
-export const data = new SlashCommandBuilder().setName('info').setDescription('Display app information');
-
-export async function execute(interaction) {
-  await interaction.deferReply({ ephemeral: true });
-  const content = "App info.";
-  await interaction.followUp({ content: content, ephemeral: true });
-};
