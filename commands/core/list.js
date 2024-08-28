914d5f53c2b2473470ff772660e4798e83c4c810
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2024-08-27 23:47:41 -0500

    remove merge markers from file

--- commands/core/list.js
+++ commands/core/list.js
@@ -7,11 +7,7 @@ const openai = new OpenAI({
 });
 
 const getAssistants = async () => {
-<<<<<<< Updated upstream
 	const assistants = await openai.beta.assistants.list();
-=======
-	const payload = await openai.beta.assistants.list();
->>>>>>> Stashed changes
 	const data = [];
 	for (let i = 0; i < assistants.data.length; i++) {
 		const text = `${assistants.data[i].name} (${assistants.data[i].model})`;
